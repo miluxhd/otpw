84d1faef3ae9130d1f268a092be0dc95b0cdd9aa
Milux Zangeneh <miluxhd@gmail.com>
2018-11-29 23:47:02 +0330

    adding base agent

---
+++ java/src/main/java/ir/milux/Agent.java
@@ -0,0 +1,32 @@
+package ir.milux;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Agent {
+    Logger logger = Logger.getLogger (Agent.class);
+    public void runServer(){
+        Server server = new Server ();
+        ServerConnector http = new ServerConnector (server);
+        http.setPort(8080);
+        server.addConnector(http);
+        ServletContextHandler handler = new ServletContextHandler(server, "/");
+        handler.addServlet (new ServletHolder (new HttpServlet () {
+            @Override
+            protected void doGet (HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException {
+                String username = req.getParameter ("Username");
+                String challenge = req.getParameter ("Challenge");
+                logger.info ("Username : " + username +",challenge : " + challenge);
+            }
+        }),"/");
+    }
+}
