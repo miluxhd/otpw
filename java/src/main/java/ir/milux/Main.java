35673015235dcfaa258375e77f134c07af70a7a2
Milux Zangeneh <miluxhd@gmail.com>
2018-12-06 19:26:57 +0330

    Add cli support

--- java/src/main/java/ir/milux/Main.java
+++ java/src/main/java/ir/milux/Main.java
@@ -1,36 +1,37 @@
 package ir.milux;
 
+import org.apache.commons.cli.*;
+
 import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
-    public static void main (String[] args) throws InterruptedException, SQLException, ClassNotFoundException {
-        ExecutorService executorService = Executors.newFixedThreadPool (10);
-        executorService.submit (new Runnable () {
-            @Override
-            public void run () {
-                Master master = new Master ();
-                try {
-                    master.runMaster ();
-                } catch (Exception e) {
-                    e.printStackTrace ();
-                }
-            }
-        });
+    public static void main (String[] args) throws Exception {
+        Options options = new Options ();
+        options.addOption("m" , "master", false, "master");
+        options.addOption ("a" , "agent",false , "agent");
+        CommandLineParser parser = new DefaultParser ();
+        CommandLine cmd = null;
+        HelpFormatter formatter = new HelpFormatter();
 
-        Thread.sleep (5000);
-        executorService.submit (new Runnable () {
-            @Override
-            public void run () {
+        try {
+            cmd = parser.parse( options, args);
+        }catch (Exception e){
+            formatter.printHelp("java -jar otpw-service.jar ", options);
+            System.exit (1);
+        }
+        if (cmd.hasOption ("master") || cmd.hasOption ("m")){
+            Master master = new Master ();
+            master.runMaster ();
+        }
+        else if (cmd.hasOption ("agent") || cmd.hasOption ("a")){
             Agent agent = new Agent ();
-                try {
-                    agent.startLocalService ();
-                } catch (Exception e) {
-                    e.printStackTrace ();
-                }
-            }
-        });
-
+            agent.startLocalService ();
+        }
+        else {
+            formatter.printHelp("java -jar otpw-service.jar ", options);
+            System.exit (1);
+        }
     }
 }
