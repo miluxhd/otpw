edd30d0a64b59d58a8df96b5ac79acde97e8b18f
Milux Zangeneh <miluxhd@gmail.com>
2018-12-07 18:59:17 +0330

    Add support for external command

--- java/src/main/java/ir/milux/MessageSender.java
+++ java/src/main/java/ir/milux/MessageSender.java
@@ -2,9 +2,17 @@ package ir.milux;
 
 import org.apache.log4j.Logger;
 
+import java.io.IOException;
+
 public class MessageSender {
     Logger logger = Logger.getLogger (MessageRequest.class);
     public void send(String id,String password){
-        logger.info ("id : " + id + "password : " + password);
+        Runtime runtime = Runtime.getRuntime ();
+        String messageCommand = Properties.getProperty ("master.messagecommand") +" '" + id + "' '" + password+"'";
+        try {
+            runtime.exec (messageCommand);
+        } catch (IOException e) {
+            logger.error (e.getMessage ());
+        }
     }
 }
